c5f1af937697d114fbf5b21cc3656efba0e1297f
DonNinja <yngvileo@gmail.com>
2022-02-21 18:38:58 +0000

    I fyekin deleted my old branch :(

---
+++ AGDDPlatformer-Polarity-Base-master/Assets/Scripts/TeleporterScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AGDDPlatformer {
+    public class TeleporterScript : MonoBehaviour {
+        [SerializeField] GameObject exit;
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            collision.transform.position = exit.transform.position + collision.transform.localScale.y * exit.transform.up;
+            PlayerController pc = collision.GetComponent<PlayerController>();
+
+            pc.velocity = -pc.velocity;
+        }
+    }
+}
