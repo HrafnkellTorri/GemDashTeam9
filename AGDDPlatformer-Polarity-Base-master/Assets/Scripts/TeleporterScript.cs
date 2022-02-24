91f390f6ba34bf7d2ab287537a91ec92f807ee34
DonNinja <yngvileo@gmail.com>
2022-02-24 21:33:54 +0000

    Finished levels :)

--- AGDDPlatformer-Polarity-Base-master/Assets/Scripts/TeleporterScript.cs
+++ AGDDPlatformer-Polarity-Base-master/Assets/Scripts/TeleporterScript.cs
@@ -7,10 +7,25 @@ namespace AGDDPlatformer {
         [SerializeField] GameObject exit;
 
         private void OnTriggerEnter2D(Collider2D collision) {
-            collision.transform.position = exit.transform.position + collision.transform.localScale.y * exit.transform.up;
-            PlayerController pc = collision.GetComponent<PlayerController>();
+            if (exit) {
 
-            pc.velocity = -pc.velocity;
+                PlayerController pc = collision.GetComponent<PlayerController>();
+
+                if (pc) {
+                    // Offset so that player shows up at a position that makes sense
+                    Vector3 y_offset = new Vector3(0, collision.transform.localScale.y / 2, 0) * pc.gravityModifier;
+                    collision.transform.position = exit.transform.position - y_offset + (collision.transform.localScale.y * exit.transform.up);
+
+                    pc.velocity = -pc.velocity;
+
+                    if (exit.transform.up != new Vector3(1, 0) && exit.transform.up != new Vector3(-1, 0))
+                        pc.velocity *= exit.transform.up;
+
+                    pc.velocity *= pc.gravityModifier;
+
+                    pc.SetJumpBoost(new Vector2(exit.transform.up.x, 0) * pc.velocity.magnitude * pc.gravityModifier);
+                }
+            }
         }
     }
 }
