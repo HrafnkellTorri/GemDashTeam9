8c5346444c6c5b1dd8dfbd2d3a3902471a00a7ec
DonNinja <yngvileo@gmail.com>
2022-03-02 16:51:24 +0000

    Made Background music loop reasonably

--- AGDDPlatformer-Polarity-Base-master/Assets/Scripts/MusicScript.cs
+++ AGDDPlatformer-Polarity-Base-master/Assets/Scripts/MusicScript.cs
@@ -3,8 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicScript : MonoBehaviour {
+    [SerializeField] AudioSource music;
+    bool fade_out = false;
+
     // Start is called before the first frame update
     void Start() {
+        if (GameObject.FindGameObjectsWithTag("MusicPlayer").Length > 1) {
+            Destroy(gameObject);
+        }
+
         DontDestroyOnLoad(this);
+        music.time = 90f;
+    }
+
+    private void Update() {
+        if (!music.isPlaying) {
+            music.Play();
+            music.time = 13.3f;
+            StartCoroutine(FadeIn(music, 3.0f, 0.5f));
+            fade_out = false;
+        }
+        else if (music.time > 93f && !fade_out) {
+            Debug.Log("Starting fade out");
+            StartCoroutine(FadeOut(music, 3.0f, 0.02f));
+            fade_out = true;
+        }
+    }
+
+    public static IEnumerator FadeOut(AudioSource audioSource, float fade_time, float target_volume) {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > target_volume) {
+            audioSource.volume -= startVolume * Time.deltaTime / fade_time;
+
+            yield return null;
+        }
+    }
+
+    public static IEnumerator FadeIn(AudioSource audioSource, float fade_time, float target_volume) {
+        while (audioSource.volume < target_volume) {
+            audioSource.volume += 0.5f * Time.deltaTime / fade_time;
+
+            yield return null;
+        }
     }
 }
